375af9d0185cac57f8d4c1a26264ed13414e764e
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-12-16 14:24:28 +0100

    Update factory.py

--- src/tracktor/datasets/factory.py
+++ src/tracktor/datasets/factory.py
@@ -4,11 +4,20 @@ from .mot_siamese_wrapper import MOT_Siamese_Wrapper
 from .mot15_wrapper import MOT15_Wrapper
 from .marcuhmot import MarCUHMOT
 
+from .football_wrapper import Football_Wrapper
+
 
 _sets = {}
 
 
 # Fill all available datasets, change here to modify / add new datasets.
+
+for split in ['train', 'test', 'all']:
+    for dets in ['']:
+        name = f'football_{split}_{dets}'
+        _sets[name] = (lambda *args, split=split,
+                       dets=dets: Football_Wrapper(split, dets, *args))
+
 for split in ['train', 'test', 'all', '01', '02', '03', '04', '05', '06', '07', '08', '09',
               '10', '11', '12', '13', '14']:
     for dets in ['DPM16', 'DPM_RAW16', 'DPM17', 'FRCNN17', 'SDP17', '17', '']:
