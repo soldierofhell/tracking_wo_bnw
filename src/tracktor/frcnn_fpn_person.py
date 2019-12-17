6eac3a4a94a0580de8d8f447d276c4e9ec35dfa7
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-12-17 11:19:58 +0100

    Update frcnn_fpn_person.py

--- src/tracktor/frcnn_fpn_person.py
+++ src/tracktor/frcnn_fpn_person.py
@@ -78,7 +78,7 @@ class FRCNN_FPN(FasterRCNN):
         
         pred_boxes = resize_boxes(
             pred_boxes, images.image_sizes[0], original_image_sizes[0])
-        pred_scores = pred_scores[:, 1].squeeze(dim=1).detach()
+        pred_scores = pred_scores[:, 1].detach() # .squeeze(dim=1)
         return pred_boxes[pred_masks], pred_scores[pred_masks]
 
     def load_image(self, img):
