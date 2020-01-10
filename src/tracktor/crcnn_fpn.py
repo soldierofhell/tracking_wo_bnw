c967e4f9ca039dd91e5eae5e7ab77d9e30977e78
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-10 11:56:19 +0100

    Update crcnn_fpn.py

--- src/tracktor/crcnn_fpn.py
+++ src/tracktor/crcnn_fpn.py
@@ -26,30 +26,20 @@ class CRCNN_FPN():
         device = list(self.model.parameters())[0].device
         img = img.to(device)
         
-        inputs = {"image": img, "height": img.size(0), "width": img.size(1)}   
-        instances = self.predictor(inputs)["instances"]
+        inputs = {"image": img, "height": img.size(0), "width": img.size(1)}
+        with torch.no_grad():
+            instances = self.predictor(inputs)["instances"]
 
         return instances.pred_boxes.detach(), instances.scores.detach()
 
     def predict_boxes(self, images, boxes):
         
-        # similar to DefaultPredictor.__call__()
-
-        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
-            # Apply pre-processing to image.
-            if self.input_format == "RGB":
-                # whether the model expects BGR inputs or RGB
-                original_image = original_image[:, :, ::-1]
-            height, width = original_image.shape[:2]
-            image = self.predictor.transform_gen.get_transform(original_image).apply_image(original_image)
-            
-            # BGR -> RGB
-            
-            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
-
-            inputs = {"image": image, "height": height, "width": width}
-            predictions = self.model([inputs])[0]
-            return predictions
+        device = list(self.model.parameters())[0].device
+        img = img.to(device)
+        
+        inputs = {"image": img, "height": img.size(0), "width": img.size(1)}
+        with torch.no_grad():
+            instances = self.predictor(inputs)["instances"]
         
         pred_scores = pred_scores[:, 1:].squeeze(dim=1).detach()
         return pred_boxes, pred_scores
