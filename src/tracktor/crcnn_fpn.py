f027d073e9131a7e6febc8702964e165ebcee077
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2020-01-11 18:05:14 +0100

    Update crcnn_fpn.py

--- src/tracktor/crcnn_fpn.py
+++ src/tracktor/crcnn_fpn.py
@@ -22,6 +22,7 @@ class CRCNN_FPN():
         self.model.cuda()
         
         self.proposal_generator = self.model.proposal_generator
+        self.test_nms_thresh = cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST
         
         checkpointer = DetectionCheckpointer(self.model)
         checkpointer.load(cfg.MODEL.WEIGHTS)
@@ -29,6 +30,7 @@ class CRCNN_FPN():
     def detect(self, img, img_size):
         
         self.model.proposal_generator = self.proposal_generator
+        self.model.roi_heads.test_nms_thresh = self.test_nms_thresh
         
         device = list(self.model.parameters())[0].device
         img = img[0].to(device)
@@ -49,6 +51,7 @@ class CRCNN_FPN():
     def predict_boxes(self, images, boxes, img_size):
         
         self.model.proposal_generator = None
+        self.model.roi_heads.test_nms_thresh = 1.0
         
         # proposals = detectron2.structures.Instances + .proposal_boxes + .objectness_logits
         # https://github.com/facebookresearch/detectron2/blob/555200fa414e7cd35ad35a00a3e98f0545f5832e/detectron2/modeling/proposal_generator/rpn_outputs.py#L147
@@ -66,7 +69,7 @@ class CRCNN_FPN():
         
         inputs = {"image": img, "height": height, "width": width, "proposals": proposals}
         with torch.no_grad():
-            instances = self.model.inference([inputs], do_postprocess=False)[0]["instances"]
+            instances = self.model.inference([inputs], do_postprocess=True)[0]["instances"]
             
             pred_boxes = instances.pred_boxes.tensor.detach()
             pred_scores = instances.scores.detach()
